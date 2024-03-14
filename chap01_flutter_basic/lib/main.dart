1710f37b055b9fd2ed5372f9cae1c40496e0027d
thdrkfka <ssongjiaran@gmail.com>
2024-03-14 12:12:01 +0900

    chap01_flutter_basic 사진 추가, 스크롤 ,input

--- chap01_flutter_basic/lib/main.dart
+++ chap01_flutter_basic/lib/main.dart
@@ -10,56 +10,73 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'App 타이틀',
-      // App의 전반적인 테마를 설정한다.
-      theme: ThemeData(
-        textTheme: TextTheme(
-          // 텍스트의 style을 미리 설정하고 호출해 사용 가능하다.
-          bodyText1: TextStyle(
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
+      debugShowCheckedModeBanner: false, // debut 띠 삭제 : ctrl + space => debug~
 
       home: Scaffold(
         appBar: AppBar(
           title: Text('AppBar'),
           backgroundColor: Colors.blue,
         ),
-
-        // Body
-        body: Container(
-          color: Colors.yellow,
+        body: SingleChildScrollView(
           child: Center(
-            child: Text('Body 입니다.'),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        Container(
+                          width: 100,
+                          height: 100,
+                          color: Colors.purple,
+                          child: Text(
+                            '컨테이너',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          alignment: Alignment.center,
+                          margin: EdgeInsets.all(20), // 박스 바깥쪽 영역
+                          padding: EdgeInsets.only(bottom: 20),
+                        ),
+                        Text(
+                          'hello',
+                          style: TextStyle(
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.yellow),
+                        ),
+                      ],
+                    ),
+                    Icon(
+                      Icons.airplane_ticket,
+                      color: Colors.lightGreen,
+                      size: 100,
+                    ),
+                  ],
+                ),
+                Image.asset(
+                  'assets/images/cat.jpg',
+                ),
+                Image.network(
+                    'https://gratisography.com/wp-content/uploads/2023/10/gratisography-cool-cat-800x525.jpg'),
+                Image.network(
+                    'https://gratisography.com/wp-content/uploads/2023/05/gratisography-colorful-cat-free-stock-photo-800x525.jpg'),
+                TextField(
+                  decoration: InputDecoration(labelText: 'Input'),
+                  // 입력폼(text)에 값이 변경될 경우 작동한다.
+                  onChanged: (text) {
+                    print(text);
+                  },
+                  // 엔터를 눌렀을 경우 작동한다.
+                  onSubmitted: (text) {
+                    print("enter를 눌렀습니다. 입력값 : $text");
+                  },
+                ),
+              ],
+              // children
+            ),
           ),
         ),
-
-        // BottomNavigationBar
-        bottomNavigationBar: BottomNavigationBar(
-          items: <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.business),
-              label: 'business',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.school),
-              label: 'school',
-            ),
-          ],
-        ),
-
-        //FloatingAcionButton
-        floatingActionButton: FloatingActionButton(
-            child: Icon(Icons.access_alarm),
-            onPressed: () => {
-                  print('hello'),
-                }),
       ),
     );
   }
