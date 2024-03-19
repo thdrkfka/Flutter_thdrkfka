bd4afc5a738b14d4fea6de29ef95b9587f6f71f5
thdrkfka <ssongjiaran@gmail.com>
2024-03-19 12:52:11 +0900

    chap06_flutter_number_trivia flutter_api 호출 연습하기_1 통신패키지 DIO

--- chap06_flutter_number_trivia/lib/main.dart
+++ chap06_flutter_number_trivia/lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,119 +9,116 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+// 홈 페이지
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+// 홈 페이지의 상태 클래스
+class _HomePageState extends State<HomePage> {
+  String quiz = '퀴즈';
 
-  void _incrementCounter() {
+  /**
+   * initState()
+   * StatefulWidget에서 위젯이 처음 생성될 때, 처음 실행되는 함수
+   */
+  @override
+  void initState() {
+    super.initState();
+    // 퀴즈 정보 불러와서 화면 갱신하기
+    getQuiz();
+  }
+
+  // Numbers API 호출하기
+  Future<String> getNumbertrivia() async {
+    String path = 'http://numbersapi.com/random/trivia';
+    Response result = await Dio().get(path);
+
+    String trivia = result.data;
+    print(trivia);
+
+    return trivia;
+  }
+
+  // 버튼을 눌렀을 때, 동작해서 새로운 퀴즈 나오는 거
+  // 재사용하기 위해 분리
+  void getQuiz() async {
+    String trivia = await getNumbertrivia();
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      quiz = trivia;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: Container(
+        color: Colors.indigo,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            /**
+             * 크로스 축 이란?
+             * "사용하는 위젯의 주축의 반대 되는 축"을 '크로스 축'이라고 한다.
+             * Column의 주축 => 세로 방향, 크로스 축 => 가로 방향
+             */
+            // CrossAxisAlignment.stretch : 크로스 축 방향으로 가능한 많은 공간을 차지하게 함.
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              /**
+               * Expanded
+               * 레이아웃 위젯
+               * 자식 위젯이 사용 가능한 추가 공간을 모두 차지하도록 확장시키는 역할
+               * 주로 Row, Column과 같은 레이아웃 위젯을 사용할 때,
+               * 내부의 자식 위젯들 사이의 공간을 동적으로 분배할 목적으로 사용됨.
+               */
+              Expanded(
+                child: Center(
+                  child: Text(
+                    quiz,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 35,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+
+              // 퀴즈 생성 버튼
+              SizedBox(
+                height: 42,
+                child: ElevatedButton(
+                  onPressed: () {
+                    // 버튼을 눌렀을 때 동작 // 얘 분리해서 재사용 할 예정
+                    getQuiz();
+                  },
+                  child: Text(
+                    'New Quiz',
+                    style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.indigo,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
