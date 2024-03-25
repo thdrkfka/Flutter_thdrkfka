ba68bf8c2e17a915f9ffe97b9b750df6c0c6b5ce
thdrkfka <ssongjiaran@gmail.com>
2024-03-25 12:06:16 +0900

    first commit

---
+++ chap08_flutter_firebase_todolist/lib/main.dart
@@ -0,0 +1,126 @@
+import 'dart:js_interop_unsafe';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+// 로그인 페이지
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
+// 홈 페이지
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  TextEditingController jobController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('TodoList'),
+        actions: [
+          TextButton(
+            onPressed: () {
+              // 로그아웃 버튼을 눌렀을 때 로그인 페이지로 이동
+            },
+            child: Text(
+              '로그아웃',
+              style: TextStyle(color: Colors.black),
+            ),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: jobController,
+                    decoration: InputDecoration(hintText: 'job을 입력해주세요.'),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    // add 버튼을 눌렀을 때 job을 추가
+                  },
+                  child: Icon(Icons.add),
+                ),
+              ],
+            ),
+          ),
+          Divider(
+            height: 1,
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                String job = '$index';
+                bool isDone = false;
+
+                return ListTile(
+                  title: Text(
+                    job,
+                    style: TextStyle(
+                        fontSize: 24,
+                        color: isDone ? Colors.grey : Colors.black,
+                        decoration: isDone
+                            ? TextDecoration.lineThrough
+                            : TextDecoration.none),
+                  ),
+                  trailing: IconButton(
+                    onPressed: () {
+                      // 삭제 버튼 눌렀을 때 동작
+                    },
+                    icon: Icon(CupertinoIcons.delete),
+                  ),
+                  onTap: () {
+                    // 아이템을 클릭했을 때, isDone 상태 변경
+                  },
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
