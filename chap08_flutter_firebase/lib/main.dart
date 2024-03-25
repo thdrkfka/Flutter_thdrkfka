c3c37ea94ae8b994a9f90070c6c636c8828f8455
thdrkfka <ssongjiaran@gmail.com>
2024-03-25 11:57:39 +0900

    first commit

--- chap08_flutter_firebase/lib/main.dart
+++ chap08_flutter_firebase/lib/main.dart
@@ -11,7 +11,78 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+      home: HomePage(),
+    );
+  }
+}
+
+// 로그인 페이지
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
+// 홈 페이지
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  TextEditingController jobController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('TodoList'),
+        actions: [
+          TextButton(
+            onPressed: () {
+              // 로그아웃 버튼을 눌렀을 때 로그인 페이지로 이동
+            },
+            child: Text(
+              '로그아웃',
+              style: TextStyle(color: Colors.black),
+            ),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: jobController,
+                    decoration: InputDecoration(hintText: 'job을 입력해주세요.'),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Icon(Icons.add),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
